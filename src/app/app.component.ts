bb15baab2e020647a258107d1d84ab5dfbe60330
andela-mmichael <mel.michael007@gmail.com>
2016-11-30 18:16:41 +0100

    Implement delete functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,11 @@ export class AppComponent {
       body: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+    },
+    {
+      title: 'Snippet 3',
+      body: `Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
     }
   ]
 
@@ -44,5 +49,9 @@ export class AppComponent {
 
   saveNote(note) {
     this.editing = false;
+  }
+
+  deleteNote(note, i) {
+    this.allNotes.splice(i,1);
   }
 }
